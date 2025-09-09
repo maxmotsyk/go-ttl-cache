c303441703ee4b40785258ab90ce4e89ff5b3461
MacBook <21mechanic21@gmail.com>
2025-09-09 21:36:43 +0300

    add some actions cache struct

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"github.com/maxmotsyk/go-ttl-cache/internal/cache"
 )
 
 func main() {
-	fmt.Println("hello")
+	cache := cache.New()
+	cache.Set("userId", 22)
+	fmt.Println(cache.Get("userId"))
+	cache.Deleat("userId")
+	fmt.Println(cache.Get("userId"))
+
 }
