38a9e37e3a11256776117f930c1e7f0da53ff937
MacBook <21mechanic21@gmail.com>
2025-09-10 21:34:56 +0300

    add RWmutex

--- cmd/main.go
+++ cmd/main.go
@@ -3,13 +3,53 @@ package main
 import (
 	"fmt"
 	"github.com/maxmotsyk/go-ttl-cache/internal/cache"
+	"sync"
+	"time"
 )
 
 func main() {
-	cache := cache.New()
-	cache.Set("userId", 22)
-	fmt.Println(cache.Get("userId"))
-	cache.Deleat("userId")
-	fmt.Println(cache.Get("userId"))
+	c := cache.New()
+	var wg sync.WaitGroup
 
+	// паралельно пишемо
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func(n int) {
+			defer wg.Done()
+			key := fmt.Sprintf("key-%d", n)
+			for j := 0; j < 1000; j++ {
+				_ = c.Set(key, j)
+			}
+		}(i)
+	}
+
+	// паралельно читаємо
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func(n int) {
+			defer wg.Done()
+			key := fmt.Sprintf("key-%d", n)
+			for j := 0; j < 1000; j++ {
+				_, _ = c.Get(key)
+			}
+		}(i)
+	}
+
+	// паралельно видаляємо
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func(n int) {
+			defer wg.Done()
+			key := fmt.Sprintf("key-%d", n)
+			for j := 0; j < 500; j++ {
+				_ = c.Deleat(key)
+			}
+		}(i)
+	}
+
+	wg.Wait()
+	fmt.Println("Test finished")
+
+	// залишаємо час, щоб подивитися в race detector
+	time.Sleep(1 * time.Second)
 }
